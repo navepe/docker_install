f0500555f607015e728c2e3067e7f664cb57bdf1
navepe <nave@webz.io>
2023-06-22 21:10:42 +0300

    daemon changes

--- docker_install.sh
+++ docker_install.sh
@@ -13,6 +13,8 @@ packages=(
   docker-compose-plugin
 )
 
+daemon_path='/etc/docker/daemon.json'
+
 sudo apt-get update
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
@@ -28,6 +30,21 @@ done
 sudo groupadd docker
 
 sudo usermod -aG docker ${USER}
+
+sudo touch $daemon_path
+sudo tee $daemon_path << EOF
+{
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "50m",
+    "max-file": "3"
+  }
+}
+EOF
+
+sudo chown root:root "$daemon_path"
+sudo systemctl restart docker
+
 
 sudo su - omgili << EOF 
     
